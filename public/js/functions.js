45203af39ee4ff22a88ce63e3a2b01129abdaf80
Liz Peacock <liz.peacock11@gmail.com>
2016-08-01 17:33:32 -0500

    adding new exercises woot!!

---
+++ public/js/functions.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var myNameIs = 'Liz'; // TODO: Fill in your name here.
+
+// TODO:
+// Create a function called 'sayHello' that takes a parameter 'name'.
+// When called, the function should log a message that says hello from the passed in name.
+function sayHello(name) {
+    ("hello" + name);
+    console.log ("hello " + name);
+}
+
+// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+sayHello(myNameIs);
+// Don't modify the following line
+// It generates a random number between 1 and 100 and stores it in random
+var random = Math.floor((Math.random()*100)+1);
+
+// TODO:
+// Create a function called 'isOdd' that takes a number as a parameter.
+// The function should use the ternary operator to log a message.
+// The log should tell the number passed in and whether it is odd or not.
+function isOdd(number) {
+    (number % 2 !== 0) ? console.log (number + ": number is odd") : console.log (number + ": number is even");
+}
+
+// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+isOdd(random);
+
+//takes a needle and a haystack
+//returns true if the needles in the haystack, otherwise false
+
+function stringContains (haystack, needle){
+    var index = haystack.indexOf(needle);
+    if (index == -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//take a string as input
+//return true if there is a " " in the string, otherwise false
+function hasASpace (stringToCheck) {
+    var stringHasASpace = stringContains (stringToCheck, " ");
+    return stringHasASpace;
+}
+
+//while the string has spaces
+function removeSpaces(phrase){
+
+    while (hasASpace (phrase)) {
+        phrase = phrase.replace(" ", "");
+    }
+    return phrase;
+}
+
+var result = removeSpaces("hello there Lassen!");
+console.log(result);
--- public/js/if-else.js
+++ public/js/if-else.js
@@ -1,43 +1,72 @@
 "use strict"
 
-var a = 75;
-var b = 80;
-var c = 95;
-var gradeAvg = ((a + b + c) / 3);
-	console.log (gradeAvg);
+// var a = 75;
+// var b = 80;
+// var c = 95;
+// var gradeAvg = ((a + b + c) / 3);
+// 	console.log (gradeAvg);
 
-if (gradeAvg > 80) {
-	console.log("You're Awesome!");
-} else {
-	console.log("You need more practice.")
+// if (gradeAvg > 80) {
+// 	console.log("You're Awesome!");
+// } else {
+// 	console.log("You need more practice.");
+// }
+
+//same problem, function style
+
+function grades (quiz, paper, final) {
+	var gradeAvg = ((quiz + paper + final) / 3);
+
+	if (gradeAvg > 80) {
+		return "You're Awesome!";
+	} else {
+		return "You need more practice."
+	}
 }
+console.log (grades (75, 80, 95));
 
-var cameronSpent = 180;
-var ryanSpent = 250;
-var georgeSpent = 320;
-var discount = 0.35;
-var applyDiscount = 200;
+// var cameronSpent = 180;
+// var ryanSpent = 250;
+// var georgeSpent = 320;
+// var discount = 0.35;
+// var applyDiscount = 200;
 
-if (cameronSpent > applyDiscount) {
-	var cameronTotal = (cameronSpent - (cameronSpent * discount));
-	console.log ("Cameron spent $" + cameronSpent + ", discount was applied. Final payment: $" + cameronTotal + ".");
-} else {
-	console.log ("Cameron spent $" + cameronSpent + ", discount was not applied. Final payment: $" + cameronTotal + ".");
+
+//function for figuring out discount amount
+function calculateDiscount (name, amountSpent, discountPercentage) {
+	if (amountSpent >= 200) {
+		var totalAfterDiscount = (amountSpent - (amountSpent * discountPercentage));
+		return name + " spent $" + amountSpent + ", discount was applied. Final payment: $" + totalAfterDiscount + ".";
+	} else {
+		return name + " spent $" + amountSpent + ", discount was not applied. Final Payment: $" + amountSpent + ".";
+	}
 }
+console.log (calculateDiscount ("Cameron", 180, 0.35)); 
+console.log (calculateDiscount ("Ryan", 250, 0.35));
+console.log (calculateDiscount ("George", 320, 0.35));
 
-if (ryanSpent > applyDiscount) {
-	var ryanTotal = (ryanSpent - (ryanSpent * discount));
-	console.log ("Ryan spent $" +ryanSpent + ", discount was applied. Final payment: $" + ryanTotal + ".");
-} else {
-	console.log ("Ryan spent $" + ryanSpent + ", discount was not applied. Final payment: $" + ryanTotal + ".");
-}
 
-if (georgeSpent > applyDiscount) {
-	var georgeTotal = (georgeSpent - (georgeSpent * discount));
-	console.log ("George spent $" + georgeSpent + ", discount was applied. Final payment: $" + georgeTotal + ".")
-} else {
-	console.log ("George spent $" + georgeSpent + ", discount was not applied. Final payment: $" + georgeTotal + ".")
-}
+
+// if (cameronSpent > applyDiscount) {
+// 	var cameronTotal = (cameronSpent - (cameronSpent * discount));
+// 	console.log ("Cameron spent $" + cameronSpent + ", discount was applied. Final payment: $" + cameronTotal + ".");
+// } else {
+// 	console.log ("Cameron spent $" + cameronSpent + ", discount was not applied. Final payment: $" + cameronTotal + ".");
+// }
+
+// if (ryanSpent > applyDiscount) {
+// 	var ryanTotal = (ryanSpent - (ryanSpent * discount));
+// 	console.log ("Ryan spent $" +ryanSpent + ", discount was applied. Final payment: $" + ryanTotal + ".");
+// } else {
+// 	console.log ("Ryan spent $" + ryanSpent + ", discount was not applied. Final payment: $" + ryanTotal + ".");
+// }
+
+// if (georgeSpent > applyDiscount) {
+// 	var georgeTotal = (georgeSpent - (georgeSpent * discount));
+// 	console.log ("George spent $" + georgeSpent + ", discount was applied. Final payment: $" + georgeTotal + ".")
+// } else {
+// 	console.log ("George spent $" + georgeSpent + ", discount was not applied. Final payment: $" + georgeTotal + ".")
+// }
 
 var flipACoin = Math.floor(Math.random()* 2);
 
@@ -46,3 +75,17 @@ if (flipACoin == 0) {
 } else {
 	console.log ("buy a house");
 }
+
+//same problem, function style
+function flipACoin (){
+	var flipACoin = Math.floor(Math.random()* 2);
+	return flipACoin;
+}
+function houseOrCar () {
+	if (flipACoin () == 0) {
+		console.log ("buy a car");
+	} else {
+		console.log ("buy a house");
+	}
+}
+
