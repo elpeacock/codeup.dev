e98960095bf8df7ee203196ffed03215a0e37fb7
Liz Peacock <liz.peacock11@gmail.com>
2016-08-09 19:09:19 -0500

    modified calculator files + morning debugging exercise

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,81 +1,144 @@
 "use strict"
 // (function (){
-//add event listeners to each number button that will input numbers to first or third text boxes
+
+////////////add event listeners to each number button/////////////////// 
+
 var numbers = document.getElementsByClassName('number');
 for (var i = 0; i < numbers.length; i += 1) {
-    numbers[i].addEventListener("click", clickNumber);
+    numbers[i].addEventListener("click", clickNumber);    
 }
+
+/////////////////////getting numbers into left/right input/////////////
 
 var leftInput = document.getElementById("leftInput");
 var rightInput = document.getElementById("rightInput");
-//move into writing number values into left input
-function clickNumber () {
+
+function clickNumber() {
     var buttonValue = this.value;
-    console.log (buttonValue);
     if (operatorInput.value === "") {
-        leftInput.value = leftInput.value + parseFloat(buttonValue);
+        leftInput.value = leftInput.value + buttonValue;
     } else {
-        rightInput.value = rightInput.value + parseFloat(buttonValue);
+        rightInput.value = rightInput.value + buttonValue;
+    }
+    // if (leftInput.value = total && operatorInput.value === ""){
+    //     leftInput.value = buttonValue;
+    // }
+}
+
+////////////////////////////Decimal button////////////////////////////
+
+document.getElementById("decimal").addEventListener("click", addDecimal);
+
+function addDecimal() {
+    if (leftInput.value.indexOf(".") == -1 && leftInput.value !== "") {
+        leftInput.value = leftInput.value + ".";
+    } else if (rightInput.value.indexOf(".") == -1 && operatorInput.value !== "" && rightInput.value !== "") {
+        rightInput.value = rightInput.value + ".";
     }
 }
-//add event listeners to each operator that will input operator to middle text box
+/////////////////add event listeners to each operator/////////////////////////
+/////////////////function to add operator to operator input///////////////////
+
 var operatorInput = document.getElementById("operatorInput");
 
-var clickAdd = function () {
+function clickAdd() {
     operatorInput.value = "+";
 }
 document.getElementById("add").addEventListener("click", clickAdd);
 
-var clickSubtract = function () {
+function clickSubtract() {
     operatorInput.value = "-";
 }
 document.getElementById("subtract").addEventListener("click", clickSubtract);
 
-var clickMultiply = function () {
+function clickMultiply() {
     operatorInput.value = "x";
 }
 document.getElementById("multiply").addEventListener("click", clickMultiply);
 
-var clickDivide = function () {
+function clickDivide() {
     operatorInput.value = "/";
 }
 document.getElementById("divide").addEventListener("click", clickDivide);
 
-//make an equals function
+function clickPercent() {
+    operatorInput.value = "%";
+}
+document.getElementById("percent").addEventListener("click", clickPercent);
+
+function clickSquareRoot() {
+    operatorInput.value = "√";
+}
+document.getElementById("squareRoot").addEventListener("click", clickSquareRoot);
+
+////////////////////////make an equals function/////////////////////////////
+
 document.getElementById("total").addEventListener("click", clickEqual);
 
-function clickEqual () {
+function clickEqual() {
     var operator = document.getElementById("total");
     var total;
     switch (operatorInput.value) {
         case "+": 
-            total = parseFloat(leftInput.value) + parseFloat(rightInput.value);
+            total = parseFloat(leftInput.value).toFixed(5) + parseFloat(rightInput.value).toFixed(5);
             break; 
         case "-": 
-            total = parseFloat(leftInput.value) - parseFloat(rightInput.value);
+            total = parseFloat(leftInput.value).toFixed(5) - parseFloat(rightInput.value).toFixed(5);
             break;
         case "x":
-            total = parseFloat(leftInput.value) * parseFloat(rightInput.value);
+            total = parseFloat(leftInput.value).toFixed(5) * parseFloat(rightInput.value).toFixed(5);
             break;
         case "/":
-            total = parseFloat(leftInput.value) / parseFloat(rightInput.value);
             if (rightInput.value == 0) {
-                total = "cannot divide by 0";
+                alert("cannot divide by zero!");
+                total = "";
+                return;
             }   
+            total = parseFloat(leftInput.value).toFixed(5) / parseFloat(rightInput.value).toFixed(5);
+            break;
+        case "√":
+            if (leftInput.value < 0) {
+                alert ("square root of a negative number does not exist!");
+                total = "";
+                return;
+            }
+            total = Math.sqrt(parseFloat(leftInput.value).toFixed(5));
+            break;
+        case "%":
+            if (leftInput.value > 0 && leftInput.value < 1) {
+                total = parseFloat(leftInput.value) * 100;
+            } else if (leftInput.value > 1) {
+                total = parseFloat(leftInput.value).toFixed(2) / 100;
+            }
             break;
     }
-    console.log (total);
     operatorInput.value = "";
     rightInput.value = "";
     leftInput.value = total; 
 }
 
-document.getElementById("clear").addEventListener("click", clear)
-function clear () {
+////////////////////////////Clear Button///////////////////////////////
+
+document.getElementById("clear").addEventListener("click", clear);
+
+function clear() {
     leftInput.value = "";
     rightInput.value = "";
     operatorInput.value = "";
 }
+
+////////////////////////////Negate or Reverse Negative/////////////////
+
+document.getElementById("plusMinus").addEventListener("click", plusMinus);
+
+function plusMinus() {
+    if (leftInput.value != 0 && operatorInput.value == "") {
+        leftInput.value = parseFloat(leftInput.value).toFixed(5) * (-1);
+    } else if (rightInput.value != 0 && operatorInput.value != "") {
+        rightInput.value = parseFloat(rightInput.value).toFixed(5) * (-1);
+    }
+}
+
 //console.log every number/button w/an event listener
 // function clickButton(){
 //     console.log(this.value);
