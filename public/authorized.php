f6d5580128d9c88b010095cefb5e8d1f748863d6
Liz Peacock <liz.peacock11@gmail.com>
2016-09-09 15:23:04 -0500

    adding $_POST exercise

---
+++ public/authorized.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Authorized</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher:400,700" rel="stylesheet">
+    <style type="text/css">
+        body {
+            font-family:'Philosopher', sans-serif;
+            background-image: url(/img/brightsquiggly.jpg);
+            background-size: cover;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        .holdTheHeaders {
+            background-color: #C70039;
+            padding-bottom: 30px;
+            padding-top: 30px;
+            margin-top: 50px;
+            border-radius: 15px;
+            width: 450px;
+            text-align: center;
+            box-shadow: 0px 0px 120px honeydew, 
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="holdTheHeaders">
+            <h1>Proceed....</h1>
+            <h2>You have been authorized</h2>
+        </div>
+
+    </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- public/login_form.php
+++ public/login_form.php
@@ -1,6 +1,18 @@
 <?php
-	var_dump($_GET);
-	var_dump($_POST);
+    
+    $message = 'Enter your login info';
+
+    if (!empty($_GET) || !empty($_POST)) {
+
+        if (($_POST['username'] === 'guest') && ($_POST['password'] === 'password')) {
+            header('location: /authorized.php');
+            die;
+        } else {
+            $message = "Login failed. Try again";
+        }
+    }
+
+
 ?>
 <!-- - build a login form, call it login_form.php
     - username or email input with placeholder
@@ -10,30 +22,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Form</title>
+    <title>Login Form</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher:400,700" rel="stylesheet">
+    <style type="text/css">
+        body {
+            font-family: 'Philosopher', sans-serif;
+            background-image: url(/img/brightsquiggly.jpg);
+            background-size: cover;
+        }
+
+        form {
+            text-align: center;
+            background-color: #C70039;
+            padding-bottom: 40px;
+            margin-top: 50px;
+            border-radius: 15px;
+            box-shadow: 0px 0px 120px honeydew;
+        }
+    </style>
 </head>
 <body>
-	<h1>Login Form</h1>
-	<form method="POST" action="/login_form.php">
-		<p>
-		<label>Username
-			<input id="username" name="username" type="text" placeholder="Username goes here">
-		</label>
-		</p>
-		
-		<p>
-		<label>Password
-			<input id="password" name="password" type="password" placeholder="Enter your password">
-		</label>
-		</p>
-		
-		<p>
-		<label>
-			<input type="checkbox" name="rememberme" id="rememberme" value="yes" checked>Remember Me
-		</label>
-		</p>
-		
-		<button type="submit">Log In!</button>
-	</form>
+    <div class="container">
+        <form class="col-md-4" method="POST" action="/login_form.php">
+            <h1><?= $message ?></h1>
+            <p>
+            <label>Username
+                <input class="form-control" id="username" name="username" type="text" placeholder="Username goes here">
+            </label>
+            </p>
+            
+            <p>
+            <label>Password
+                <input class="form-control" id="password" name="password" type="password" placeholder="Enter your password">
+            </label>
+            </p>
+            
+            <button class="btn btn-secondary"type="submit">Log In!</button>
+        </form>
+        <p></p>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
