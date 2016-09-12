6d7c6fff387a337def94dfbdb71f2a99bf15db2a
Liz Peacock <liz.peacock11@gmail.com>
2016-09-12 17:27:59 -0500

    made snips of header/footer/navbar to requrire/include in php
    put togher the snips in the requireINclude.php

---
+++ public/header.php
@@ -0,0 +1,22 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="personal page">
+    <meta name="author" content="Elizabeth Peacock">
+
+    <title>Elizabeth Peacock</title>
+
+    <!--google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,700,700i" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/personal.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
