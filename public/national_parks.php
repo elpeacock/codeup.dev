0492110853d17259058bf3565d7044e0262c98bd
Liz Peacock <liz.peacock11@gmail.com>
2016-09-23 17:00:31 -0500

    began styling

--- public/national_parks.php
+++ public/national_parks.php
@@ -9,6 +9,9 @@ const DB_PASS = 'parks';
 //req db_connect
 require_once '../db_connect.php';
 
+//echo connection status
+// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
 
 
 
@@ -52,52 +55,61 @@ extract(pageController($dbc));
 <head>
     <title>National Parks</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style type="text/css">
+        body {
+            background-image: url('img/GrandPrismatic.jpeg');
+            background-size: cover;
+        }
+        .container {
+            background-color: 
+
+        }
+        #pages {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
-        <p>
-
-            <table>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Date Established</th>
-                <th>Area In Acres</th>
-                <?php foreach ($parks as $index => $park) : ?>
-                    <tr>
-                        <td><?= $park['name'] ?></td>
-                        <td><?= $park['location'] ?></td>
-                        <td><?= $park['date_established'] ?></td>
-                        <td><?= $park['area_in_acres'] ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        </p>
-    </div>
-    <div id="footer">
-        <div class="container">
-
+        <div class="table-container">
+            <h1>National Parks</h1>
+                <table class="table">
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Date Established</th>
+                    <th>Area In Acres</th>
+                    <?php foreach ($parks as $index => $park) : ?>
+                        <tr>
+                            <td><?= $park['name'] ?></td>
+                            <td><?= $park['location'] ?></td>
+                            <td><?= $park['date_established'] ?></td>
+                            <td><?= $park['area_in_acres'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+        </div>
+        <div id="footer">
             <div id="pages">
                 <?php
-                if($currentPage > 1)
+                if ($currentPage > 1)
                 {
-                    echo "<a href='?page=".($currentPage-1)." '>Previous  </a> ";
+                    echo "<a href='?page=" . ($currentPage - 1) . " '>Previous  </a> ";
                 }
-                for ($i=1; $i<=($total/4); $i++)
-                    if($i == $currentPage)
+                for ($i = 1; $i <= ($total/4); $i += 1)
+                    if ($i == $currentPage)
                     {
                         echo $i;
                     } else {
-                        echo "<a href='?page=".($i)." '>"." " .$i." "."</a>";
+                        echo "<a href='?page=" . ($i) . " '>" . " " . $i . " " . "</a>";
                     }
-                    if($currentPage < $total)
+                    if ($currentPage < $total)
                     {
-                        echo "<a href='?page=".($currentPage+1)." '>  Next</a> ";
+                        echo "<a href='?page=" . ($currentPage + 1) . " '>  Next</a> ";
                     } 
                     ?>
-
                 </div>
             </div>
-        </div>
+    </div>
 
 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
