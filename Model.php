35da3ed89178057f1e6952a0c9d941d489d99761
Liz Peacock <liz.peacock11@gmail.com>
2016-09-30 16:46:47 -0500

    committing first 2 steps of Model.php abstract

---
+++ Model.php
@@ -0,0 +1,98 @@
+<?php
+
+abstract class Model
+{
+    /** @var PDO|null Connection to the database */
+    protected static $dbc = null;
+
+    /** @var array Database values for a single record. Array keys should be column names in the DB */
+    protected $attributes = array();
+
+
+    /**
+     * Constructor
+     *
+     * An instance of a class derived from Model represents a single record in the database.
+     *
+     * $param array $attributes Optional array of database values to initialize the model record with
+     */
+    public function __construct(array $attributes = array())
+    {
+        self::dbConnect();
+
+        // @TODO: Initialize the $attributes property with the passed value
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Connect to the DB
+     *
+     * This method should be called at the beginning of any function that needs to communicate with the database
+     */
+    protected static function dbConnect()
+    {
+        if (!self::$dbc) {
+            // @TODO: Connect to database
+            // in the file where Model.php is being included
+                // define('DB_HOST', '127.0.0.1');
+                // define('DB_NAME', 'whatever_the_database_to_use_is_named');
+                // define('DB_USER', 'my_username_for_the_database_to_use');
+                // define('DB_PASS', 'my_password_for_the_database_to_use');
+
+            // Get new instance of PDO object ->hafta override the default value of null defined above so use static:: instead of self::
+            static::$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+
+            // Tell PDO to throw exceptions on error ->again hafta override the default value of null use static:: not self::
+            static::$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    /**
+     * Get a value from attributes based on its name
+     *
+     * @param string $name key for attributes array
+     *
+     * @return mixed|null value from the attributes array or null if it is undefined
+     */
+    public function __get($name)
+    {
+        // @TODO: Return the value from attributes for $name if it exists, else return null
+        if (condition) {
+            # code...
+        }
+
+    }
+
+    /**
+     * Set a new value for a key in attributes
+     *
+     * @param string $name  key for attributes array
+     * @param mixed  $value value to be saved in attributes array
+     */
+    public function __set($name, $value)
+    {
+        // @TODO: Store name/value pair in attributes array
+    }
+
+    /** Store the object in the database */
+    public function save()
+    {
+        // @TODO: Ensure there are values in the attributes array before attempting to save
+
+        // @TODO: Call the proper database method: if the `id` is set this is an update, else it is a insert
+    }
+
+    /**
+     * Insert new entry into database
+     *
+     * NOTE: Because this method is abstract, any child class MUST have it defined.
+     */
+    protected abstract function insert();
+
+    /**
+     * Update existing entry in database
+     *
+     * NOTE: Because this method is abstract, any child class MUST have it defined.
+     */
+    protected abstract function update();
+}
