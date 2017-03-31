09f0a14c12d0819a12f49324c49e40056a177ec8
Liz Peacock <liz.peacock11@gmail.com>
2017-03-31 13:51:01 -0500

    working with Olympic - refactored again to use the Auth class

--- public/logout.php
+++ public/logout.php
@@ -2,26 +2,26 @@
 //continue open session    
 session_start();
 //bring in Auth class
-// require_once '../Auth.php';
+require_once '../Auth.php';
 //call Auth logout fxn
-// Auth::logout();
+Auth::logout();
 
-function clearSession()
-{
-    // clear $_SESSION array
-    session_unset();
+// function clearSession()
+// {
+//     // clear $_SESSION array
+//     session_unset();
 
-    // delete session data on the server
-    session_destroy();
+//     // delete session data on the server
+//     session_destroy();
 
-    // ensure client is sent a new session cookie
-    session_regenerate_id();
+//     // ensure client is sent a new session cookie
+//     session_regenerate_id();
 
-    // start a new session - session_destroy() ended our previous session so
-    // if we want to store any new data in $_SESSION we must start a new one
-    session_start();
-}   
-clearSession();
+//     // start a new session - session_destroy() ended our previous session so
+//     // if we want to store any new data in $_SESSION we must start a new one
+//     session_start();
+// }   
+// clearSession();
 ?>
 <!DOCTYPE html>
 <html>
